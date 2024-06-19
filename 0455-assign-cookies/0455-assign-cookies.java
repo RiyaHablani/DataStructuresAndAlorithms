1a4c5fae44ba8da2281b416a849fa6bbe7f1285b
RiyaHablani <121542860+RiyaHablani@users.noreply.github.com>
2024-06-19 14:50:31 +0530

    Time: 8 ms (97.83%), Space: 45.3 MB (21.52%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.java
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -2,19 +2,11 @@ class Solution {
     public int findContentChildren(int[] g, int[] s) {
         Arrays.sort(g);
         Arrays.sort(s);
+        int i = 0;
         
-        int child = 0;
-        int cookie = 0;
-        
-        while (child < g.length && cookie < s.length) {
-            if (s[cookie] >= g[child]) {
-                // The current cookie can satisfy the current child's greed factor
-                child++;
-            }
-            // Move to the next cookie
-            cookie++;
+        for(int j=0;i<g.length && j<s.length;j++) {
+	        if(g[i]<=s[j]) i++;
         }
-        
-        return child;
+        return i;
     }
 }
