744fad01c4f9287669996e966f9489ef94043c92
RiyaHablani <121542860+RiyaHablani@users.noreply.github.com>
2024-08-01 21:48:50 +0530

    Time: 2 ms (99.24%), Space: 48.1 MB (27.67%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,19 +1,35 @@
 class Solution {
-    public List<Integer> majorityElement(int[] nums) {
-        HashMap<Integer,Integer> map=new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            if(map.containsKey(nums[i])){
-                map.put(nums[i],map.get(nums[i])+1);
-            }else{
-                map.put(nums[i],1);
+    public List<Integer> majorityElement(int[] v) {
+        int n = v.length;
+
+        int cnt1 = 0, cnt2 = 0; 
+        int el1 = Integer.MIN_VALUE; 
+        int el2 = Integer.MIN_VALUE; 
+
+        for (int i = 0; i < n; i++) {
+            if (cnt1 == 0 && el2 != v[i]) {
+                cnt1 = 1;
+                el1 = v[i];
+            } else if (cnt2 == 0 && el1 != v[i]) {
+                cnt2 = 1;
+                el2 = v[i];
+            } else if (v[i] == el1) cnt1++;
+            else if (v[i] == el2) cnt2++;
+            else {
+                cnt1--; cnt2--;
             }
         }
-        List<Integer> ans = new ArrayList<>();
-        for(int k: map.keySet()){
-            if(map.get(k)>nums.length/3){
-                ans.add(k);
-            }
+
+        List<Integer> ls = new ArrayList<>(); 
+        cnt1 = 0; cnt2 = 0;
+        for (int i = 0; i < n; i++) {
+            if (v[i] == el1) cnt1++;
+            if (v[i] == el2) cnt2++;
         }
-        return ans;
+        int mini = (int)(n / 3) + 1;
+        if (cnt1 >= mini) ls.add(el1);
+        if (cnt2 >= mini) ls.add(el2);
+
+        return ls;
     }
 }
