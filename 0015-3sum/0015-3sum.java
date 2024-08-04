311377e63813e4dfbee95232960d9ad5c64188f3
RiyaHablani <121542860+RiyaHablani@users.noreply.github.com>
2024-08-04 15:03:55 +0530

    Time: 31 ms (59.05%), Space: 51.2 MB (84.67%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,22 +1,35 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        Set<List<Integer>> set=new HashSet<>();
-        if(nums.length==0) return new ArrayList<>();
+        //-1 -1 0 1 2 4
+        List<List<Integer>> result = new ArrayList<>();
+        if (nums == null || nums.length < 3) {
+            return result;
+        }
         Arrays.sort(nums);
-        int sum=0;
-        for(int i=0;i<nums.length-2;i++)
-        {
-            int j=i+1;
-            int k=nums.length-1;
-            while(j<k)
-            {
-                sum=nums[i]+nums[j]+nums[k];
-                if(sum==0) set.add(Arrays.asList(nums[i],nums[j++],nums[k--]));
-                if(sum<0) j++;
-                if(sum>0) k--;
-
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue; // Skip duplicate numbers
+            }
+            int left = i + 1, right = nums.length - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0) {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    while (left < right && nums[left] == nums[left + 1]) {
+                        left++; // Skip duplicate numbers
+                    }
+                    while (left < right && nums[right] == nums[right - 1]) {
+                        right--; // Skip duplicate numbers
+                    }
+                    left++;
+                    right--;
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
             }
         }
-        return new ArrayList<>(set);
+        return result;
     }
 }
