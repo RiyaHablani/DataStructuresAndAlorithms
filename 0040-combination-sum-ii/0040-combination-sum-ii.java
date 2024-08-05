4e79d6584724bae82ba5f35f5d37803c6058aa46
RiyaHablani <121542860+RiyaHablani@users.noreply.github.com>
2024-08-05 13:31:27 +0530

    Time: 4 ms (46.41%), Space: 43.3 MB (23.78%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,21 +1,27 @@
 class Solution {
-    public List<List<Integer>> combinationSum2(int[] nums, int target) {
-        List<List<Integer>> list = new ArrayList<>();
-        Arrays.sort(nums); // Sort the input array to handle duplicates
-        backtrack(list, new ArrayList<>(), nums, target, 0);
-        return list;
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        Arrays.sort(candidates);
+        List<List<Integer>> result = new ArrayList<>();
+        findSum(0, candidates,target, new ArrayList<>(), result);
+        return result;
     }
-
-    private void backtrack(List<List<Integer>> list, List<Integer> tempList, int[] nums, int remain, int start) {
-        if (remain < 0) return; // If remaining sum becomes negative, terminate
-        else if (remain == 0) list.add(new ArrayList<>(tempList)); // If remaining sum becomes zero, add the current combination to the list
-        else {
-            for (int i = start; i < nums.length; i++) {
-                if (i > start && nums[i] == nums[i - 1]) continue; // Skip duplicates
-                tempList.add(nums[i]);
-                backtrack(list, tempList, nums, remain - nums[i], i + 1); // Recur with updated sum and start index
-                tempList.remove(tempList.size() - 1); // Backtrack by removing the last added element
+    public static void findSum(int indx,int[] candidates,int target,List<Integer> ans,List<List<Integer>> result){
+        if (target == 0) {
+            List<Integer> combination = new ArrayList<>(ans);
+            result.add(combination);
+            return;
+        }
+        if(target<0){
+            return;
+        }
+        for(int i=indx;i<candidates.length;i++){
+             // Skip duplicates
+            if (i > indx && candidates[i] == candidates[i - 1]) {
+                continue;
             }
+            ans.add(candidates[i]);
+            findSum(i+1,candidates,target-candidates[i],ans,result);
+            ans.remove(ans.size()-1);
         }
     }
 }
