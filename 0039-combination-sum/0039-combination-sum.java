813bee7071baa288a093ebf48a4e2acb7baef771
RiyaHablani <121542860+RiyaHablani@users.noreply.github.com>
2024-08-05 13:17:01 +0530

    Time: 2 ms (84.34%), Space: 44.6 MB (46.09%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,21 +1,22 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-       List<List<Integer>> ans=new ArrayList<>();
-       List<Integer> ds=new ArrayList<>();
-       solve(0,candidates,target,ans,ds);
-       return ans;
+        List<List<Integer>> result = new ArrayList<>();
+        findSum(0, candidates,target, new ArrayList<>(), result);
+        return result;
     }
-    void solve(int start,int[] candidates,int target,List<List<Integer>> ans,List<Integer> ds){
-        if(target==0){
-            ans.add(new ArrayList<>(ds));
+    static void findSum(int indx, int[] candidates, int target,List<Integer> ans, List<List<Integer>>result) {
+        if (target == 0) {
+            List<Integer> combination = new ArrayList<>(ans);
+            result.add(combination);
             return;
         }
-        if(target<0)
-          return;
-        for(int i=start;i<candidates.length;i++){
-            ds.add(candidates[i]);
-            solve(i,candidates,target-candidates[i],ans,ds);
-            ds.remove(ds.size()-1);
+        if(target<0){
+            return;
         }
+        for(int i=indx;i<candidates.length;i++){
+            ans.add(candidates[i]);
+            findSum(i,candidates,target-candidates[i],ans,result);
+            ans.remove(ans.size()-1);
+        }        
     }
 }
