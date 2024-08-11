a85b11ad8fbc98c9a98142e147e47c08e8cdf349
RiyaHablani <121542860+RiyaHablani@users.noreply.github.com>
2024-08-11 13:29:06 +0530

    Time: 2 ms (82.82%), Space: 45.8 MB (64.99%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int nonzeroindex=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=0){
+                nums[nonzeroindex]=nums[i];
+                nonzeroindex++;
+            }
+        }
+        for(int j=nonzeroindex;j<nums.length;j++){
+            nums[j]=0;
+        }
+    }
+}
