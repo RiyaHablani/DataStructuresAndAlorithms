daa9b8101d94d1d661b18cd9c2780bc986679bbe
RiyaHablani <121542860+RiyaHablani@users.noreply.github.com>
2024-09-22 20:17:32 +0530

    Time: 5 ms (71.03%), Space: 43.7 MB (78.23%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,24 +1,18 @@
 class Solution {
-    public boolean isPalindrome(int n) {
-        int original = n;
-        int temp = 0;
-        n = Math.abs(n);  // Convert the number to positive
-        
-        // Reverse the number
-        while (n > 0) {
-            int digit = n % 10;
-            temp = temp * 10 + digit;
-            n = n / 10;
-        }
-        
-        if (original < 0) {
-            String reversedWithMinus = temp + "-";
-            System.out.println(reversedWithMinus);  
-        }
-        if (original == temp) {
-            return true;
-        } else {
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
             return false;
         }
+
+        long reversed = 0;
+        long temp = x;
+
+        while (temp != 0) {
+            int digit = (int) (temp % 10);
+            reversed = reversed * 10 + digit;
+            temp /= 10;
+        }
+
+        return (reversed == x);
     }
 }
