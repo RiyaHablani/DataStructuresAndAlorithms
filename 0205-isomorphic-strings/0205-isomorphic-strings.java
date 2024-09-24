78ee1df62bd66751f79ffcb9556939f8c0ddc541
RiyaHablani <121542860+RiyaHablani@users.noreply.github.com>
2024-09-24 22:07:01 +0530

    Time: 6 ms (92.11%), Space: 42.5 MB (26.27%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,25 +1,25 @@
 class Solution {
     public boolean isIsomorphic(String s1, String s2) {
-        HashMap<Character,Character> map1= new HashMap<>();
-        HashMap<Character,Character> map2= new HashMap<>();
+        if (s1.length() != s2.length()) return false;
         
-        if(s1.length()!=s2.length()) return false;
-        for(int i=0;i<s1.length();i++){
-            if(!map1.containsKey(s1.charAt(i))){
-                map1.put(s1.charAt(i),s2.charAt(i));
-            }else{
-                if(!map1.get(s1.charAt(i)).equals(s2.charAt(i))){
-                    return false;
-                }
+        // Use arrays of size 256 (ASCII character set size)
+        int[] map1 = new int[256];
+        int[] map2 = new int[256];
+        
+        for (int i = 0; i < s1.length(); i++) {
+            char c1 = s1.charAt(i);
+            char c2 = s2.charAt(i);
+            
+            // If either of the mappings is inconsistent, return false
+            if (map1[c1] != map2[c2]) {
+                return false;
             }
-            if(!map2.containsKey(s2.charAt(i))){
-                map2.put(s2.charAt(i),s1.charAt(i));
-            }else{
-                if(!map2.get(s2.charAt(i)).equals(s1.charAt(i))){
-                    return false;
-                }
-            }
+            
+            // Set both mappings to the current index + 1 (to avoid conflicts with default 0)
+            map1[c1] = i + 1;
+            map2[c2] = i + 1;
         }
+        
         return true;
     }
 }
