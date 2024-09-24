1364cf47607ce0850d2917cf6b9aa6c7772e270e
RiyaHablani <121542860+RiyaHablani@users.noreply.github.com>
2024-09-24 22:33:20 +0530

    Time: 4 ms (73.67%), Space: 44.8 MB (20.23%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -3,18 +3,12 @@ class Solution {
         if (s.length() != t.length()) {
             return false;
         }
-        HashMap<Character, Integer> map = new HashMap<>();
-        for (int i = 0; i < s.length(); i++) {
-            char charS = s.charAt(i);
-            map.put(charS, map.getOrDefault(charS, 0) + 1);
-            char charT = t.charAt(i);
-            map.put(charT, map.getOrDefault(charT, 0) - 1);
-        }
-        for (int count : map.values()) {
-            if (count != 0) {
-                return false;
-            }
-        }
-        return true;
+        char[] sArray = s.toCharArray();
+        char[] tArray = t.toCharArray();
+        
+        Arrays.sort(sArray);
+        Arrays.sort(tArray);
+        
+        return Arrays.equals(sArray, tArray);
     }
 }
