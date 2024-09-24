ab845b0c53a78d7455fe39bab7698aacacb461a3
RiyaHablani <121542860+RiyaHablani@users.noreply.github.com>
2024-09-24 19:34:23 +0530

    Time: 1 ms (34.38%), Space: 42.2 MB (45.53%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,26 +1,38 @@
+import java.util.Arrays;
+
 class Solution {
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int i=m-1,j=n-1,k=m+n-1;
-        while(i>=0 && j>=0){
-            if(nums1[i]>nums2[j]){
-                nums1[k]=nums1[i];
-                i--;
-                k--;
-            }else{
-                nums1[k]=nums2[j];
-                k--;
-                j--;
+    public void merge(int[] arr1, int m, int[] arr2, int n) {
+        int len = m + n;
+        int gap = (len / 2) + (len % 2);
+        while (gap > 0) {
+            int left = 0;
+            int right = left + gap;
+
+            while (right < len) {
+                if (left < m && right >= m) {
+                    swapIfGreater(arr1, arr2, left, right - m);
+                }
+                else if (left >= m) {
+                    swapIfGreater(arr2, arr2, left - m, right - m);
+                }
+                else {
+                    swapIfGreater(arr1, arr1, left, right);
+                }
+                left++;
+                right++;
             }
+            if (gap == 1) break;
+            gap = (gap / 2) + (gap % 2);
         }
-        while(i>=0){
-            nums1[k]=nums1[i];
-            i--;
-            k--;
+        for (int i = 0; i < n; i++) {
+            arr1[m + i] = arr2[i];
         }
-        while(j>=0){
-            nums1[k]=nums2[j];
-            j--;
-            k--;
+    }
+    public static void swapIfGreater(int[] arr1, int[] arr2, int ind1, int ind2) {
+        if (arr1[ind1] > arr2[ind2]) {
+            int temp = arr1[ind1];
+            arr1[ind1] = arr2[ind2];
+            arr2[ind2] = temp;
         }
     }
 }
