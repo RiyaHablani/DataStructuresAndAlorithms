ef7ddd9febed442a00004ceaa9e7006f8b0c6c0d
RiyaHablani <121542860+RiyaHablani@users.noreply.github.com>
2024-09-25 13:25:04 +0530

    Time: 263 ms (46.34%), Space: 42.1 MB (76.45%) - LeetHub

--- 0686-repeated-string-match/0686-repeated-string-match.java
+++ 0686-repeated-string-match/0686-repeated-string-match.java
@@ -1,27 +1,19 @@
 class Solution {
     public int repeatedStringMatch(String a, String b) {
-        StringBuilder repeatedA = new StringBuilder(a);
-        int count = 1;
-
-        // Repeat string `a` until its length exceeds or matches the length of `b`
-        while (repeatedA.length() < b.length()) {
-            repeatedA.append(a);
+        StringBuilder sb= new StringBuilder();
+        int count=0;
+        while(sb.length()<b.length()){
+            sb.append(a);
             count++;
         }
-
-        // Check if `b` is now a substring of the repeated `a`
-        if (repeatedA.indexOf(b) != -1) {
+        if(sb.indexOf(b)!=-1){
             return count;
         }
-
-        // Check one more repetition of `a`
-        repeatedA.append(a);
+        sb.append(a);
         count++;
-        if (repeatedA.indexOf(b) != -1) {
+        if(sb.indexOf(b)!=-1){
             return count;
         }
-
-        // If `b` is still not a substring, return -1
         return -1;
     }
 }
