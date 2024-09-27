d0533514261b9e618fff79ea26a5e2bbbbe2a5bd
RiyaHablani <121542860+RiyaHablani@users.noreply.github.com>
2024-09-27 14:23:47 +0530

    Time: 13 ms (94.95%), Space: 58.5 MB (38.46%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,13 +1,45 @@
 class Solution {
-    public int findKthLargest(int[] nums, int k) {
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>();  // default is min-heap
+    public void swap(int[] nums, int i, int j) {
+		int temp = nums[i];
+		nums[i] = nums[j];
+		nums[j] = temp;
+	}
 
-        for (int num : nums) {
-            minHeap.add(num);
-            if (minHeap.size() > k) { 
-                minHeap.remove();
-            }
-        }
-        return minHeap.peek();
+	public int quickSelect(int[] nums, int left, int right, int k) {
+		int p = left + ((right - left) / 2);
+		if (right > left) {
+			int r2 = right;
+			while (left != right) {
+				if (left != p) {
+					if (nums[left] > nums[p]) {
+						swap(nums, left, p);
+						p = left;
+					} else {
+						left++;
+					}
+				}
+				if (right != p) {
+					if (nums[right] < nums[p]) {
+						swap(nums, right, p);
+						p = right;
+					} else {
+						right--;
+					}
+				}
+			}
+			if ((nums.length - k) == p) {
+				return nums[p];
+			} else {
+				if ((nums.length - k) < p) {
+					return quickSelect(nums, 0, p - 1, k);
+				} else {
+					return quickSelect(nums, p + 1, r2, k);
+				}
+			}
+		}
+		return nums[p];
+	}
+    public int findKthLargest(int[] nums, int k) {
+        return quickSelect(nums, 0, nums.length -1, k);
     }
 }
