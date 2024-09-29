c75e268973e9455d641ee3f566cad0609aa7d399
RiyaHablani <121542860+RiyaHablani@users.noreply.github.com>
2024-09-29 16:59:55 +0530

    Time: 12 ms (71.44%), Space: 44.7 MB (40.12%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.java
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.java
@@ -1,27 +1,14 @@
 class Solution {
     public int numberOfSubstrings(String s) {
-        int[] count = new int[3]; // count[0] for 'a', count[1] for 'b', count[2] for 'c'
-        int left = 0, right = 0, result = 0;
-        int n = s.length();
+        int[] lastseen = new int[3];
+        Arrays.fill(lastseen, -1);
+        int ans = 0;
+        
+        for (int i = 0; i < s.length(); i++) {
+            lastseen[s.charAt(i) - 'a'] = i;
 
-        while (right < n) {
-            // Increment the count of the character at the right pointer
-            count[s.charAt(right) - 'a']++;
-
-            // Check if the current window contains at least one of each 'a', 'b', and 'c'
-            while (count[0] > 0 && count[1] > 0 && count[2] > 0) {
-                // If valid, add the number of substrings that end at the current right pointer
-                result += n - right;
-
-                // Move the left pointer to reduce the window size and try to find other valid windows
-                count[s.charAt(left) - 'a']--;
-                left++;
-            }
-
-            // Move the right pointer to expand the window
-            right++;
+            ans += 1 + Math.min(lastseen[0], Math.min(lastseen[1], lastseen[2]));
         }
-
-        return result;
+        return ans;
     }
 }
