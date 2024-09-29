c482986208dd3b29b5ca1eca969a09400d8cffd1
RiyaHablani <121542860+RiyaHablani@users.noreply.github.com>
2024-09-29 18:06:58 +0530

    Time: 16 ms (23.82%), Space: 55.2 MB (34.1%) - LeetHub

--- 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.java
+++ 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.java
@@ -1,21 +1,36 @@
 class Solution {
     public int numberOfSubarrays(int[] nums, int k) {
-        int end=0,start=0,n=nums.length,res=0,odd=0,count=0;
-        while(end<n){
-            if(nums[end]%2==1){
-                odd++;
-                count=0;
+        for (int num : nums) {
+            if (num % 2 == 1) {
+                num = 1;
+            } else {
+                num = 0;
             }
-            while(odd==k){
-                count++;  // it is counting still the subarray is nice (means it has odd numbers equal to k)
-                if(nums[start]%2==1){
-                    odd--;
-                }
-                start++;
-            }
-            res+=count; // will add the result here 
-            end++;
         }
-        return res;
+
+        return numSubarraysWithSum(nums, k);
+    }
+
+    public int numSubarraysWithSum(int[] nums, int goal) {
+        return helper(nums, goal) - helper(nums, goal - 1);
+    }
+
+    public int helper(int[] nums, int goal) {
+        if (goal < 0)
+            return 0;
+        int l = 0;
+        int r = 0;
+        int sum = 0;
+        int count = 0;
+        while (r < nums.length) {
+            sum = sum + (nums[r] % 2);
+            while (sum > goal) {
+                sum = sum - (nums[l] % 2);
+                l++;
+            }
+            count += r - l + 1;
+            r++;
+        }
+        return count;
     }
 }
