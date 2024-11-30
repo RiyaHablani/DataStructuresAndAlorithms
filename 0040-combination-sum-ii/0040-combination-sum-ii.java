e7f3d3ab313484eff60c8f8b4e33504a6cbf3904
RiyaHablani <121542860+RiyaHablani@users.noreply.github.com>
2024-11-30 20:11:48 +0530

    Time: 3 ms (79.01%), Space: 43.6 MB (9.38%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,27 +1,25 @@
 class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        Arrays.sort(candidates);
         List<List<Integer>> result = new ArrayList<>();
-        findSum(0, candidates,target, new ArrayList<>(), result);
+        List<Integer> current = new ArrayList<>();
+        Arrays.sort(candidates);
+        combinationSumHelper(0, candidates, current, result, target);
         return result;
     }
-    public static void findSum(int indx,int[] candidates,int target,List<Integer> ans,List<List<Integer>> result){
+
+    private void combinationSumHelper(int index, int[] candidates, List<Integer> current, List<List<Integer>> result, int target) {
         if (target == 0) {
-            List<Integer> combination = new ArrayList<>(ans);
-            result.add(combination);
+            result.add(new ArrayList<>(current)); // Found a valid combination
             return;
         }
-        if(target<0){
-            return;
-        }
-        for(int i=indx;i<candidates.length;i++){
-             // Skip duplicates
-            if (i > indx && candidates[i] == candidates[i - 1]) {
-                continue;
+
+        for (int i = index; i < candidates.length; i++) {
+            if (i > index && candidates[i] == candidates[i - 1]) continue;
+            if (candidates[i] <= target) {
+                current.add(candidates[i]); // Choose the candidate
+                combinationSumHelper(i+1, candidates,current, result, target - candidates[i]); // Recur with updated target
+                current.remove(current.size() - 1); // Backtrack
             }
-            ans.add(candidates[i]);
-            findSum(i+1,candidates,target-candidates[i],ans,result);
-            ans.remove(ans.size()-1);
-        }
+        }  
     }
 }
