6fe7f4fc751e0eef8c94ae3dddc977a7b7b42af9
RiyaHablani <121542860+RiyaHablani@users.noreply.github.com>
2024-12-01 18:04:46 +0530

    Time: 0 ms (100%), Space: 42.3 MB (39.37%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,22 +1,20 @@
 class Solution {
     public double myPow(double x, int n) {
-        if (n == 0) return 1;
-        if (x == 0) return 0;
+        if (n == 0) return 1.0; // Edge case: anything to the power of 0 is 1.
         
-        long N = n;
-        if (N < 0) {
+        long N = n; // Handle edge case where n = Integer.MIN_VALUE
+        if (n < 0) {
             x = 1 / x;
             N = -N;
         }
-        
-        double result = 1;
+
+        double result = 1.0;
         while (N > 0) {
-            if ((N & 1) == 1) {//n%2==1
-                result *= x;
-            }
-            x *= x;
-            N >>= 1;//n=n/2
+            if ((N & 1) == 1) result *= x;  // If the current bit is 1, multiply result by x
+            x *= x;  // Square the base
+            N >>= 1; // Right shift n
         }
+
         return result;
     }
 }
