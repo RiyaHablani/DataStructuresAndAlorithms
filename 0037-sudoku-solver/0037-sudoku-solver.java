f924a5950ffe265770d61cbf23dfb3229e513d73
RiyaHablani <121542860+RiyaHablani@users.noreply.github.com>
2024-12-01 12:30:25 +0530

    Time: 19 ms (24.18%), Space: 39.9 MB (99.98%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -1,36 +1,37 @@
 class Solution {
     public void solveSudoku(char[][] board) {
-    for (int i = 0; i < 9; i++) {
-      for (int j = 0; j < 9; j++) {
-        if (board[i][j] == '.') {
-          for (char c = '1'; c <= '9'; c++) {
-            if (isValid(board, i, j, c)) {
-              board[i][j] = c;
+        solve(board);
+    }
+    public boolean solve(char[][]board){
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
 
-              if (solveSudoku(board))
-                return true;
-              else
-                board[i][j] = '.';
+                if(board[i][j]=='.'){
+
+                    for(char num='1';num<='9';num++){
+                        if(isPossible(board,i,j,num)){
+                            board[i][j]=num;
+                            if(solve(board)){
+                                return true;
+                            }
+                            else{
+                                board[i][j]='.';
+                            }
+                        }
+                    }
+                    return false;
+                }
             }
-          }
-          return false;
         }
-      }
+        return true;
     }
-    return true;
-  }
 
-  public static boolean isValid(char[][] board, int row, int col, char c) {
-    for (int i = 0; i < 9; i++) {
-      if (board[i][col] == c)
-        return false;
-
-      if (board[row][i] == c)
-        return false;
-
-      if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c)
-        return false;
-    }
-    return true;
+    public boolean isPossible(char[][]board,int row,int col,char c){
+        for(int i=0;i<9;i++){
+            if(board[i][col]==c) return false;
+            if(board[row][i]==c) return false;
+            if(board[3*(row/3)+i/3][3*(col/3)+i%3]==c) return false;//checking in each 3 x 3 matrix
+        }
+        return true;
     }
 }
