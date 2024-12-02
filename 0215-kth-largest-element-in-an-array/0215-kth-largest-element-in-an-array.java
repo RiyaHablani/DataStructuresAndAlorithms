5375fcf7653f88cc24e4985d1a5fb4a020aae308
RiyaHablani <121542860+RiyaHablani@users.noreply.github.com>
2024-12-02 19:49:21 +0530

    Time: 41 ms (66.26%), Space: 57.2 MB (84.39%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,45 +1,31 @@
-class Solution {
-    public void swap(int[] nums, int i, int j) {
-		int temp = nums[i];
-		nums[i] = nums[j];
-		nums[j] = temp;
-	}
+import java.util.PriorityQueue;
 
-	public int quickSelect(int[] nums, int left, int right, int k) {
-		int p = left + ((right - left) / 2);
-		if (right > left) {
-			int r2 = right;
-			while (left != right) {
-				if (left != p) {
-					if (nums[left] > nums[p]) {
-						swap(nums, left, p);
-						p = left;
-					} else {
-						left++;
-					}
-				}
-				if (right != p) {
-					if (nums[right] < nums[p]) {
-						swap(nums, right, p);
-						p = right;
-					} else {
-						right--;
-					}
-				}
-			}
-			if ((nums.length - k) == p) {
-				return nums[p];
-			} else {
-				if ((nums.length - k) < p) {
-					return quickSelect(nums, 0, p - 1, k);
-				} else {
-					return quickSelect(nums, p + 1, r2, k);
-				}
-			}
-		}
-		return nums[p];
-	}
+public class Solution {
     public int findKthLargest(int[] nums, int k) {
-        return quickSelect(nums, 0, nums.length -1, k);
+        // Create a min-heap (PriorityQueue). By default, a PriorityQueue is a min-heap in Java.
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        
+        // Step 1: Add the first 'k' elements of the array to the min-heap
+        // This ensures that we have a heap of size 'k' containing the 'k' largest elements so far.
+        for (int i = 0; i < k; i++) {
+            minHeap.offer(nums[i]);  // Offer each number to the min-heap (heapify them)
+        }
+        
+        // Step 2: Iterate through the rest of the array, starting from the k-th element
+        for (int i = k; i < nums.length; i++) {
+            // If the current number is greater than the smallest number in the heap
+            // (which is at the root of the min-heap), then we need to replace it.
+            if (nums[i] > minHeap.peek()) {
+                // Step 3: Remove the smallest element (root of the heap)
+                minHeap.poll();  
+                // Step 4: Add the current element (nums[i]) into the min-heap
+                minHeap.offer(nums[i]);
+            }
+        }
+        
+        // Step 5: The root of the min-heap is the kth largest element
+        // The heap size will be exactly 'k' after processing all the elements,
+        // and the root will be the kth largest element.
+        return minHeap.peek();  // Peek returns the root (smallest element) of the min-heap
     }
 }
