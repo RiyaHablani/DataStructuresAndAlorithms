d7981641eda7cce30b5e55bec956d01a7ce3de18
RiyaHablani <121542860+RiyaHablani@users.noreply.github.com>
2024-12-04 21:56:21 +0530

    Time: 9 ms (90.11%), Space: 44.7 MB (75.49%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,20 +1,37 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        char sc[] = s.toCharArray();
-        char pc[] = p.toCharArray();
+        List<Integer> result = new ArrayList<>();
+        
+        // Base case: if p is longer than s, no anagrams are possible
+        if (s.length() < p.length()) return result;
 
-        List<Integer> res = new ArrayList<>();
-        Arrays.sort(pc);
+        // Frequency arrays for 'a' to 'z'
+        int[] pCount = new int[26];
+        int[] sCount = new int[26];
 
-        int k = pc.length;
-        for (int i = 0; i <= sc.length - k; i++) {
-            String str = s.substring(i, i + k);
-            char sort[] = str.toCharArray();
-            Arrays.sort(sort);
-            if (Arrays.equals(pc, sort)) {
-                res.add(i);
+        // Populate the frequency array for p
+        for (char c : p.toCharArray()) {
+            pCount[c - 'a']++;
+        }
+
+        int windowSize = p.length();
+
+        // Traverse the string s
+        for (int i = 0; i < s.length(); i++) {
+            // Add the current character to the sCount
+            sCount[s.charAt(i) - 'a']++;
+
+            // Remove the character that is sliding out of the window
+            if (i >= windowSize) {
+                sCount[s.charAt(i - windowSize) - 'a']--;
+            }
+
+            // Compare the frequency arrays
+            if (Arrays.equals(pCount, sCount)) {
+                result.add(i - windowSize + 1); // Starting index of the anagram
             }
         }
-        return res;
+
+        return result;
     }
 }
