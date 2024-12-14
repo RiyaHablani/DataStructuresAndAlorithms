3eccc2a3d3fc3ee9d21bd2f092e882bea3af88fd
RiyaHablani <121542860+RiyaHablani@users.noreply.github.com>
2024-12-14 19:26:24 +0530

    Time: 8 ms (32.24%), Space: 44.4 MB (60.42%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.java
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.java
@@ -3,14 +3,19 @@ class Solution {
         int up=0,left=0;
         for(int i=0;i<moves.length();i++){
             char ch=moves.charAt(i);
-            if(ch=='U'){
-                up++;
-            }else if(ch=='D'){
-                up--;
-            }else if(ch=='L'){
-                left++;
-            }else if(ch=='R'){
-                left--;
+            switch(ch){
+            case 'U':
+               up++;
+               break;
+            case 'D':
+               up--;
+               break;
+            case 'L':
+               left++;
+               break;
+            case 'R':
+               left--;
+               break;
             }
         }
         return left==0 && up==0;
