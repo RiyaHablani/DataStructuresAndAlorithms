c9b4d9544222ae954ec0283035f071cac2a2072e
RiyaHablani <121542860+RiyaHablani@users.noreply.github.com>
2024-12-19 19:52:58 +0530

    Time: 0 ms (100%), Space: 40.2 MB (99.73%) - LeetHub

--- 0780-max-chunks-to-make-sorted/0780-max-chunks-to-make-sorted.java
+++ 0780-max-chunks-to-make-sorted/0780-max-chunks-to-make-sorted.java
@@ -1,19 +1,15 @@
 class Solution {
     public int maxChunksToSorted(int[] arr) {
-        int[] sortedArr = Arrays.copyOf(arr, arr.length);
-        Arrays.sort(sortedArr);
-        
-        int count = 0;
-        int sum1 = 0, sum2 = 0;
-        
+        if (arr == null || arr.length == 0)
+            return 0;
+
+        int count = 0, max = 0;
         for (int i = 0; i < arr.length; i++) {
-            sum1 += arr[i];
-            sum2 += sortedArr[i];
-            if (sum1 == sum2) {
+            max = Math.max(max, arr[i]);
+            if (max == i)
                 count++;
-            }
         }
-        
+
         return count;
     }
 }
