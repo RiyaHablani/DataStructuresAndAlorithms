2cd55971b9934c014d6c9f3e6e18563b1004ab96
RiyaHablani <121542860+RiyaHablani@users.noreply.github.com>
2025-01-17 20:44:50 +0530

    Time: 1 ms (99.9%), Space: 42.7 MB (23.86%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -1,33 +1,26 @@
 class Solution {
     public String addBinary(String a, String b) {
-        String reverseda="";
-        String reversedb="";
-        for(int i=a.length()-1;i>=0;i--){
-            reverseda+=a.charAt(i);
-        }
-        for(int i=b.length()-1;i>=0;i--){
-            reversedb+=b.charAt(i);
-        }
         StringBuilder sb=new StringBuilder();
-        int n=reverseda.length();
-        int m=reversedb.length();
+        int n=a.length(),m=b.length();
         int carry=0;
-        for(int i=0;i<n || i<m || carry!=0;i++){
+        int i=n-1,j=m-1;
+        while(i>=0 || j>=0 || carry!=0 ){
             int bitA,bitB;
-            if(i<n){
-                bitA=reverseda.charAt(i)-'0';
+            if(i>=0){
+                bitA=a.charAt(i)-'0';
             }else{
                 bitA=0;
             }
-            if(i<m){
-                bitB=reversedb.charAt(i)-'0';
+            if(j>=0){
+                bitB=b.charAt(j)-'0';
             }else{
                 bitB=0;
             }
-            int sum = bitA + bitB + carry;
-    
-            sb.append(sum % 2);  
-            carry = sum / 2; 
+             i--;
+             j--;
+            int sum=bitA+bitB+carry;
+            sb.append(sum%2);
+            carry=sum/2;
         }
         return sb.reverse().toString();
     }
