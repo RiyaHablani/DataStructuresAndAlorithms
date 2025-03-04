dac46c9ffa6365865c02568ed7fed8a5c0ad1d55
RiyaHablani <121542860+RiyaHablani@users.noreply.github.com>
2025-03-04 13:51:46 +0530

    Time: 61 ms (52.19%), Space: 61.9 MB (14.55%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,58 +1,12 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        // The kth largest element corresponds to the (n-k)th smallest index in sorted order
-        int targetIdx = nums.length - k;
-
-        // Use the Quickselect algorithm to find the target index
-        return quickSelect(nums, 0, nums.length - 1, targetIdx);
-    }
-
-    private int quickSelect(int[] nums, int left, int right, int targetIdx) {
-        // Base case: if the left and right pointers meet, we have a single element
-        if (left == right) {
-            return nums[left];
-        }
-
-        // Choose the leftmost element as the pivot
-        int pivot = nums[left];
-        int low = left;  // Pointer to move forward from the left
-        int high = right;  // Pointer to move backward from the right
-
-        // Partition the array around the pivot
-        while (low <= high) {
-            // Move the 'low' pointer to the right until finding a value >= pivot
-            while (low <= high && nums[low] < pivot) {
-                low++;
-            }
-            // Move the 'high' pointer to the left until finding a value <= pivot
-            while (low <= high && nums[high] > pivot) {
-                high--;
-            }
-            // Swap elements if 'low' is still less than or equal to 'high'
-            if (low <= high) {
-                int temp = nums[low];
-                nums[low] = nums[high];
-                nums[high] = temp;
-                low++;
-                high--;
+        PriorityQueue<Integer> minHeap= new PriorityQueue<>();
+        for(int i=0;i<nums.length;i++){
+            minHeap.offer(nums[i]);
+            if(minHeap.size()>k){
+                minHeap.poll();
             }
         }
-
-        // After partitioning:
-        // 'high' is the last index of the left (smaller) partition
-        // 'low' is the first index of the right (larger) partition
-
-        // If the target index is in the left partition, recursively search there
-        if (targetIdx <= high) {
-            return quickSelect(nums, left, high, targetIdx);
-        }
-        // If the target index is in the right partition, recursively search there
-        else if (targetIdx >= low) {
-            return quickSelect(nums, low, right, targetIdx);
-        }
-        // Otherwise, the pivot is the target element
-        else {
-            return nums[targetIdx];
-        }
+        return minHeap.peek();
     }
 }
