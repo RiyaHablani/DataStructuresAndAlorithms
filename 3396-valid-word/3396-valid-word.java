cb62ac195378b81b6543a9b1f8343038acab3b61
RiyaHablani <121542860+RiyaHablani@users.noreply.github.com>
2025-07-15 13:49:00 +0530

    Time: 1 ms (99.12%), Space: 42.2 MB (34.66%) - LeetHub

--- 3396-valid-word/3396-valid-word.java
+++ 3396-valid-word/3396-valid-word.java
@@ -1,25 +1,21 @@
 class Solution {
     public boolean isValid(String word) {
-        if(word.length()<3)
-            return false;
-        int v=0;
-        int c=0;
-        int ch=0;
-        for(int i=0;i<word.length();i++){
-            char chh=word.charAt(i);
-            if(word.charAt(i)>='a' && word.charAt(i)<='z' || word.charAt(i)>='A' && word.charAt(i)<='Z' || word.charAt(i)>='0' && word.charAt(i)<='9'){
-                ch++;
-                if(chh=='a' || chh=='e' || chh=='i' || chh=='o' || chh=='u'|| chh=='A' || chh=='E' || chh=='I' || chh=='O' || chh=='U' )
-                    v++;
-                else
-                    if(word.charAt(i)>='a' && word.charAt(i)<='z'|| word.charAt(i)>='A' && word.charAt(i)<='Z')
-                    c++;
+        int vowel = 0, consonant = 0;
+        if (word.length() < 3) return false;
+
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+
+            if (!Character.isLetterOrDigit(ch)) return false;
+
+            char lowerCh = Character.toLowerCase(ch);
+            if (lowerCh == 'a' || lowerCh == 'e' || lowerCh == 'i' || lowerCh == 'o' || lowerCh == 'u') {
+                vowel++;
+            } else if (Character.isLetter(ch)) {
+                consonant++;
             }
-            else
-                return false;
         }
-        if(v>=1 && c>=1 && ch>=3)
-            return true;
-        return false;
+
+        return vowel > 0 && consonant > 0;
     }
 }
