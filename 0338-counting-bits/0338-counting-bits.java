31867a8cb6560f069249d93e5e7ae313ac113b53
RiyaHablani <121542860+RiyaHablani@users.noreply.github.com>
2025-09-14 20:29:53 +0530

    Time: 2 ms (94.11%), Space: 46.8 MB (35.06%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,10 +1,9 @@
 class Solution {
     public int[] countBits(int n) {
-        int[] arr = new int[n + 1];  // Array to store the result for numbers from 0 to n
-        for (int i = 1; i <= n; i++) {
-            // Dynamic programming relation: count[i] = count[i >> 1] + (i & 1)
-            arr[i] = arr[i >> 1] + (i & 1);
+        int dp[]=new int[n+1];
+        for(int i=1;i<=n;i++){
+           dp[i]=dp[i>>1] + (i&1);
         }
-        return arr;
+        return dp;
     }
 }
