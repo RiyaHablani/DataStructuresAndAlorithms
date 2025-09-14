75614a1e7ac45268ce5ba1d754dccb02a962a3c9
RiyaHablani <121542860+RiyaHablani@users.noreply.github.com>
2025-09-14 21:02:45 +0530

    Time: 1 ms (38.96%), Space: 43 MB (90.24%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.java
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.java
@@ -16,25 +16,41 @@ class Solution {
     // }
   
   
-     public int minCostClimbingStairs(int[] cost) {
-       // Memorization
-       int n=cost.length;
-        int dp[]=new int[n+1];
-        Arrays.fill(dp,-1);
-        return recur(n,cost,dp);
-    }
-    public int recur(int n,int cost[],int dp[]){
-        if(n==0 || n==1){
-            dp[n]=0;
-            return 0;
-        }
-        if(dp[n]!=-1){
-            return dp[n];
-        }
-        int oneStep=cost[n-1]+recur(n-1,cost,dp);
-        int twoStep=cost[n-2]+recur(n-2,cost,dp);
+    //  public int minCostClimbingStairs(int[] cost) {
+    //    // Memorization TC:O(n)
+    //    int n=cost.length;
+    //     int dp[]=new int[n+1];
+    //     Arrays.fill(dp,-1);
+    //     return recur(n,cost,dp);
+    // }
+    // public int recur(int n,int cost[],int dp[]){
+    //     if(n==0 || n==1){
+    //         dp[n]=0;
+    //         return 0;
+    //     }
+    //     if(dp[n]!=-1){
+    //         return dp[n];
+    //     }
+    //     int oneStep=cost[n-1]+recur(n-1,cost,dp);
+    //     int twoStep=cost[n-2]+recur(n-2,cost,dp);
 
-        dp[n] = Math.min(oneStep, twoStep);
+    //     dp[n] = Math.min(oneStep, twoStep);
+    //     return dp[n];
+    // }
+
+
+      public int minCostClimbingStairs(int[] cost) {
+       // Tabulation TC:O(n)
+       int n=cost.length;
+       int dp[]=new int[n+1];
+        dp[0]=0;
+        dp[1]=0;
+        for(int i=2;i<=n;i++){
+            int oneStep=cost[i-1]+dp[i-1];
+            int twoStep=cost[i-2]+dp[i-2];
+
+            dp[i]=Math.min(oneStep,twoStep);
+        }
         return dp[n];
     }
 }
