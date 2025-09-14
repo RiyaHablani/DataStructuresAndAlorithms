3f3b154def6e76724b4ba878aa9c56821f4020ef
RiyaHablani <121542860+RiyaHablani@users.noreply.github.com>
2025-09-14 19:15:19 +0530

    Time: 0 ms (100%), Space: 40.3 MB (66.36%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -7,22 +7,42 @@ class Solution {
     //     }
     //     return climbStairs(n-1)+climbStairs(n-2);
     // }
+
+
+
+    // public int climbStairs(int n) {
+    //    //overlapping Subproblem
+    //    //Memorization- top down apporach
+    //    TC = O(n), SC = O(n)
+    //    int dp[]=new int[n+1];
+    //    Arrays.fill(dp,-1);
+    //    return recur(n,dp);
+    // }
+    // public int recur(int n,int dp[]){
+    //     if(n<=2){
+    //         dp[n]=n;
+    //         return n;
+    //     }
+    //     if(dp[n]!=-1){
+    //         return dp[n];
+    //     }
+    //     dp[n]= recur(n-1, dp) + recur(n-2, dp);
+    //     return dp[n];
+    // }
+
+
     public int climbStairs(int n) {
-       //overlapping Subproblem
-       //Memorization- top down apporach
-       int dp[]=new int[n+1];
-       Arrays.fill(dp,-1);
-       return recur(n,dp);
-    }
-    public int recur(int n,int dp[]){
-        if(n<=2){
-            dp[n]=n;
-            return n;
-        }
-        if(dp[n]!=-1){
-            return dp[n];
-        }
-        dp[n]= recur(n-1, dp) + recur(n-2, dp);
-        return dp[n];
+        //Bottom-up approach  approach-Tabulation
+      if(n<=2){
+        return n;
+      }
+      int dp[]=new int[n+1];
+      dp[1]=1;
+      dp[2]=2;
+
+      for(int state=3;state<=n;state++){
+        dp[state]=dp[state-1]+dp[state-2];
+      }
+      return dp[n];
     }
 }
