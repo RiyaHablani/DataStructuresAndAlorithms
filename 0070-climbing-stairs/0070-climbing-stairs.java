41c02fa09d54b314b8a4c7638ce2fed6c13b6f13
RiyaHablani <121542860+RiyaHablani@users.noreply.github.com>
2025-09-14 19:18:25 +0530

    Time: 0 ms (100%), Space: 40.6 MB (28.37%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -31,18 +31,37 @@ class Solution {
     // }
 
 
-    public int climbStairs(int n) {
-        //Bottom-up approach  approach-Tabulation
+    // public int climbStairs(int n) {
+    //     //Bottom-up approach  approach-Tabulation
+    //   if(n<=2){
+    //     return n;
+    //   }
+    //   int dp[]=new int[n+1];
+    //   dp[1]=1;
+    //   dp[2]=2;
+
+    //   for(int state=3;state<=n;state++){
+    //     dp[state]=dp[state-1]+dp[state-2];
+    //   }
+    //   return dp[n];
+    // }
+
+
+
+     public int climbStairs(int n) {
+        //optimization
       if(n<=2){
         return n;
       }
-      int dp[]=new int[n+1];
-      dp[1]=1;
-      dp[2]=2;
+      int prev2=1;
+      int prev1=2;
+      int ans=0;
 
       for(int state=3;state<=n;state++){
-        dp[state]=dp[state-1]+dp[state-2];
+        ans=prev1+prev2;
+        prev2=prev1;
+        prev1=ans;
       }
-      return dp[n];
+      return ans;
     }
 }
