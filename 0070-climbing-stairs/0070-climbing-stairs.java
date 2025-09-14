2e5322749d590955315827f739b9a8339321f340
RiyaHablani <121542860+RiyaHablani@users.noreply.github.com>
2025-09-14 19:01:32 +0530

    Time: 0 ms (100%), Space: 40.3 MB (66.36%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,14 +1,28 @@
 class Solution {
+    // public int climbStairs(int n) {
+    //     TC: 2^n Recursion
+    //     //base case
+    //     if(n<=2){
+    //         return n;
+    //     }
+    //     return climbStairs(n-1)+climbStairs(n-2);
+    // }
     public int climbStairs(int n) {
-        //int dp[]= new int[n+1];
-        int prev2=1;
-        int prev1=1;
-        
-        for(int i=2;i<=n;i++){
-            int curr=prev1+prev2;
-            prev2=prev1;
-            prev1=curr;
+       //overlapping Subproblem
+       //Memorization- top down apporach
+       int dp[]=new int[n+1];
+       Arrays.fill(dp,-1);
+       return recur(n,dp);
+    }
+    public int recur(int n,int dp[]){
+        if(n<=2){
+            dp[n]=n;
+            return n;
         }
-        return prev1;
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        dp[n]= recur(n-1, dp) + recur(n-2, dp);
+        return dp[n];
     }
 }
