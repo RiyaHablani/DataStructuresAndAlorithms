cbcc3564a23794a866f9bff4ca16d968941d7fde
RiyaHablani <121542860+RiyaHablani@users.noreply.github.com>
2025-11-23 17:11:42 +0530

    Time: 8 ms (6.81%), Space: 44.9 MB (20.8%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -1,32 +1,26 @@
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
         Arrays.sort(nums1);
-        ArrayList<Integer> list = new ArrayList<>();
-        boolean[] used = new boolean[nums1.length];
-
-        for (int num : nums2) {
-            int left = 0;
-            int right = nums1.length - 1;
-
-            while (left <= right) {
-                int mid = left + (right - left) / 2;
-                if (nums1[mid] < num) left = mid + 1;
-                else right = mid - 1;
+        ArrayList<Integer> list=new ArrayList<>();
+        boolean[] used =new boolean[nums1.length];
+        for(int num:nums2){
+            int left=0;
+            int right=nums1.length-1;
+            while(left<=right){
+                int mid=left+(right-left)/2;
+                if(nums1[mid]<num) left=mid+1;
+                else right=mid-1;
             }
+            int pos=left;
+            while(pos<nums1.length && nums1[pos]==num && used[pos]) pos++;
 
-            // Minimal fix: move forward from 'left' to find the next unused match (if any)
-            int pos = left;
-            while (pos < nums1.length && nums1[pos] == num && used[pos]) pos++;
-
-            if (pos < nums1.length && nums1[pos] == num) {
-                used[pos] = true;
+            if(pos<nums1.length && nums1[pos]==num){
+                used[pos]=true;
                 list.add(num);
             }
         }
-
-        int[] ans = new int[list.size()];
-        for (int i = 0; i < list.size(); i++) ans[i] = list.get(i);
-
+        int [] ans=new int[list.size()];
+        for(int i=0;i<list.size();i++) ans[i]=list.get(i);
         return ans;
     }
 }
