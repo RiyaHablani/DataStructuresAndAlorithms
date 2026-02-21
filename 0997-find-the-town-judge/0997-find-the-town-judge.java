fdfdb30e68ee54f03e10ca2670336d0b10e868fa
RiyaHablani <121542860+RiyaHablani@users.noreply.github.com>
2026-02-21 22:34:36 +0530

    Time: 2 ms (99.73%), Space: 53.4 MB (91.8%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.java
+++ 0997-find-the-town-judge/0997-find-the-town-judge.java
@@ -1,10 +1,12 @@
 class Solution {
     public int findJudge(int n, int[][] trust) {
+        if (n == 1) return 1;
+        if (trust.length < n - 1) return -1; // not enough edges to have a judge
+
         int[] score = new int[n + 1]; // in-degree minus out-degree
         for (int[] t : trust) {
-            int a = t[0], b = t[1];
-            score[a]--;   // a trusts someone -> not judge
-            score[b]++;   // b is trusted by a
+            score[t[0]]--; // trusts someone
+            score[t[1]]++; // is trusted by someone
         }
         for (int i = 1; i <= n; i++) {
             if (score[i] == n - 1) return i; // trusted by everyone else, trusts nobody
