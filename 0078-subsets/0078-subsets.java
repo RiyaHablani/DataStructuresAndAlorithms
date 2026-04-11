2b3d5a4dd6bf196ad86fd72b3e366058b75d725a
RiyaHablani <121542860+RiyaHablani@users.noreply.github.com>
2026-04-12 02:05:05 +0530

    Time: 1 ms (88.68%), Space: 44.1 MB (79.31%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,17 +1,17 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        List<Integer> current = new ArrayList<>();
-        generateSubsets(0, nums, current, result);
-        return result;
-    }
-
-    private void generateSubsets(int index, int[] nums, List<Integer> current, List<List<Integer>> result) {
-        result.add(new ArrayList<>(current)); // Add the current subset to the result
-        for (int i = index; i < nums.length; i++) {
-            current.add(nums[i]); // Include the current element
-            generateSubsets(i + 1, nums, current, result); // Recurse for the next index
-            current.remove(current.size() - 1); // Backtrack to remove the current element
+        List<List<Integer>> res=new ArrayList<>();
+        int len=nums.length;
+        int subsets=1<<len;
+        for(int i=0;i<subsets;i++){
+            List<Integer> list=new ArrayList<>();
+            for(int j=0;j<len;j++){
+                if((i&(1<<j))!=0){
+                    list.add(nums[j]);
+                }
+            }
+            res.add(list);
         }
+        return res;
     }
 }
