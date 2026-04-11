99a5340224f804b7570738c37f66606cea2a6e7b
RiyaHablani <121542860+RiyaHablani@users.noreply.github.com>
2026-04-12 00:44:30 +0530

    Time: 4 ms (63.21%), Space: 45.3 MB (67.56%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -1,10 +1,17 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int ones = 0, twos = 0;
-        for (int num : nums) {
-            ones = (ones ^ num) & ~twos;
-            twos = (twos ^ num) & ~ones;
+        int res=0;
+        for(int i=0;i<32;i++){
+            int bitCount=0;
+            for(int num:nums){
+                if((num&(1<<i))!=0){//checking if ith bit was set in num
+                    bitCount++;
+                }
+            }
+            if(bitCount%3!=0){//set the ith bit if count is not divisible by 3
+                res=res|(1<<i);
+            }
         }
-        return ones;
+        return res;
     }
 }
