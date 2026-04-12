17570e6649665c14d4de9debe26b537bcc8cc99b
RiyaHablani <121542860+RiyaHablani@users.noreply.github.com>
2026-04-12 21:13:43 +0530

    Time: 0 ms (100%), Space: 45.1 MB (34.7%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -4,30 +4,27 @@ class Solution {
         int end=nums.length-1;
         while(start<=end){
             int mid=start+(end-start)/2;
-            if(nums[mid]==target){
+            if(target==nums[mid]){
                 return true;
             }
-            if(nums[mid]==nums[start] && nums[mid]==nums[end]){
+            if(nums[start]==nums[mid] && nums[mid]==nums[end]){
                 start++;
-                end--;   
-            }//left side is sorted
+                end--;
+            }
             else if(nums[start]<=nums[mid]){
-                //can ans be found in left side
-                if(nums[start]<=target && target<nums[mid]){
+                if(target<nums[mid] && target>=nums[start]){
                     end=mid-1;
                 }else{
-                   start=mid+1;
+                    start=mid+1;
                 }
-                
-            }//right side is sorted
+            }
             else{
-                //can ans be found in right side
-                if(nums[mid] < target && target <= nums[end]){
+                if(target>nums[mid] && target<=nums[end]){
                     start=mid+1;
                 }else{
-                   end=mid-1;
-                }  
-            } 
+                    end=mid-1;
+                }
+            }
         }
         return false;
     }
