4bcae6406e311165905c8b6b0f0fc133d8762a38
RiyaHablani <121542860+RiyaHablani@users.noreply.github.com>
2026-04-12 19:04:58 +0530

    Time: 2 ms (86.46%), Space: 47.3 MB (80.58%) - LeetHub

---
+++ 0941-valid-mountain-array/0941-valid-mountain-array.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean validMountainArray(int[] arr) {
+        int index=0;
+        int n=arr.length;
+        while(index<n-1){
+            if(arr[index]<arr[index+1]){
+                index++;
+            }else{
+                break;
+            }
+        }
+        if(index==0 || index==n-1){
+            return false;
+        }
+        while(index<n-1){
+            if(arr[index]>arr[index+1]){
+                index++;
+            }else{
+                break;
+            }
+        }
+        return (index==n-1);
+    }
+}
