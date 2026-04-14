16894c512fcc78964c65d79b792bd84ebb4bb272
RiyaHablani <121542860+RiyaHablani@users.noreply.github.com>
2026-04-14 14:31:54 +0530

    Time: 1 ms (97.78%), Space: 42.8 MB (28.91%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -3,18 +3,16 @@ class Solution {
         int start=1;
         int end=x;
         while(start<=end){
-            int mid=start+(end-start)/2;//4,2,2,3
+            int mid=start+(end-start)/2;
             if(mid<=x/mid){
                 if(x%mid==0 && mid==x/mid){
                     return mid;
                 }
-                start=mid+1;//2,3
+                start=mid+1;
             }else{
-                end=mid-1;//3,2
+                end=mid-1;
             }
         }
         return end;
     }
 }
-// .....start->  <-end
-//....end    start
