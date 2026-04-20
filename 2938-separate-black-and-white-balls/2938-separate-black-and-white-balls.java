14de914c8e0e232cb731cc3f159e66bc128dcaf4
RiyaHablani <121542860+RiyaHablani@users.noreply.github.com>
2026-04-20 19:20:52 +0530

    Time: 8 ms (50.45%), Space: 47.5 MB (85.66%) - LeetHub

---
+++ 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.java
@@ -0,0 +1,14 @@
+class Solution {
+    public long minimumSteps(String s) {
+        int n=s.length();
+        int last=0;
+        long swapCount=0;
+        for(int i=0;i<n;i++){
+            if(s.charAt(i)=='0'){
+                swapCount+=(i-last);
+                last++;
+            }
+        }
+        return swapCount;
+    }
+}
