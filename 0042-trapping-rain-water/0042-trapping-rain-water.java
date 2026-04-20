0931d4af2f4c4274db279526a9500891d7ab808a
RiyaHablani <121542860+RiyaHablani@users.noreply.github.com>
2026-04-20 18:08:27 +0530

    Time: 1 ms (59.1%), Space: 47.9 MB (40.88%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,33 +1,27 @@
 class Solution {
     public int trap(int[] height) {
-        int n = height.length;
-        int left = 0;          // left pointer
-        int right = n - 1;     // right pointer
-        int leftMax = 0;       // max height to the left
-        int rightMax = 0;      // max height to the right
-        int water = 0;         // total water trapped
-
-        // Process until pointers meet
-        while (left < right) {
-            if (height[left] < height[right]) {
-                // Left is smaller, process left side
-                if (height[left] >= leftMax) {
-                    leftMax = height[left];  // update max
-                } else {
-                    water += leftMax - height[left];  // trap water
-                }
-                left++;
-            } else {
-                // Right is smaller, process right side
-                if (height[right] >= rightMax) {
-                    rightMax = height[right];  // update max
-                } else {
-                    water += rightMax - height[right];  // trap water
-                }
-                right--;
+        int leftMax=0;
+        int rightMax=0;
+        int l=0;
+        int n=height.length;
+        int r=n-1;
+        int ans=0;
+        while(l<r){
+            if(height[l]>leftMax){
+                leftMax=Math.max(leftMax,height[l]);
+            }
+            if(height[r]>rightMax){
+                rightMax=Math.max(rightMax,height[r]);
+            }
+            if(leftMax<rightMax){
+                ans=ans+leftMax-height[l];
+                l++;
+            }
+            else{
+                ans=ans+rightMax-height[r];
+                r--;
             }
         }
-
-        return water;
+        return ans;
     }
 }
