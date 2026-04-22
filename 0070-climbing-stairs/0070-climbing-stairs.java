de333abac0eca64aabb43d89d5703e416fa34ef7
RiyaHablani <121542860+RiyaHablani@users.noreply.github.com>
2026-04-22 20:24:12 +0530

    Time: 0 ms (100%), Space: 42 MB (74.76%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,67 +1,17 @@
 class Solution {
-    // public int climbStairs(int n) {
-    //     TC: 2^n Recursion
-    //     //base case
-    //     if(n<=2){
-    //         return n;
-    //     }
-    //     return climbStairs(n-1)+climbStairs(n-2);
-    // }
+    public int climbStairs(int n) {
+        if(n<=2){
+            return n;
+        }
+        int prev2=1;
+        int prev1=2;
+        int ans=0;
 
-
-
-    // public int climbStairs(int n) {
-    //    //overlapping Subproblem
-    //    //Memorization- top down apporach
-    //    TC = O(n), SC = O(n)
-    //    int dp[]=new int[n+1];
-    //    Arrays.fill(dp,-1);
-    //    return recur(n,dp);
-    // }
-    // public int recur(int n,int dp[]){
-    //     if(n<=2){
-    //         dp[n]=n;
-    //         return n;
-    //     }
-    //     if(dp[n]!=-1){
-    //         return dp[n];
-    //     }
-    //     dp[n]= recur(n-1, dp) + recur(n-2, dp);
-    //     return dp[n];
-    // }
-
-
-    // public int climbStairs(int n) {
-    //     //Bottom-up approach  approach-Tabulation
-    //   if(n<=2){
-    //     return n;
-    //   }
-    //   int dp[]=new int[n+1];
-    //   dp[1]=1;
-    //   dp[2]=2;
-
-    //   for(int state=3;state<=n;state++){
-    //     dp[state]=dp[state-1]+dp[state-2];
-    //   }
-    //   return dp[n];
-    // }
-
-
-
-     public int climbStairs(int n) {
-        //optimization
-      if(n<=2){
-        return n;
-      }
-      int prev2=1;
-      int prev1=2;
-      int ans=0;
-
-      for(int state=3;state<=n;state++){
-        ans=prev1+prev2;
-        prev2=prev1;
-        prev1=ans;
-      }
-      return ans;
+        for(int state=3;state<=n;state++){
+            ans=prev1+prev2;
+            prev2=prev1;
+            prev1=ans;
+        }
+        return ans;
     }
 }
