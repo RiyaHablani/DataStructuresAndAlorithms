06bfd1adcb0ccd2bd0b76ae40207401ba908f6b5
RiyaHablani <121542860+RiyaHablani@users.noreply.github.com>
2026-04-22 21:41:08 +0530

    Time: 0 ms (100%), Space: 42.7 MB (62.79%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,37 +1,39 @@
 class Solution {
-    public int rob1(int[] nums) {
-        //TC:O(n),SC:O(1)
+    public int rob(int[] nums) {
         int n=nums.length;
-        if(n==1) return nums[0];
-
+        int nums1[]=new int[n-1];
+        int nums2[]=new int[n-1];
+        int j=0;
+        int k=0;
+        if(n==1){
+            return nums[0];
+        }
+        for(int i=0;i<n;i++){
+            if(i!=0){
+                nums1[j]=nums[i];
+                j++;
+            }if(i!=n-1){
+                nums2[k]=nums[k];
+                k++;
+            }
+        }
+        return Math.max(rob1(nums1),rob1(nums2));
+    }
+    public int rob1(int[] nums) {
+        int n=nums.length;
+        if(n==1){
+            return nums[0];
+        }
         int prev2=0;
         int prev1=nums[0];
         int ans=0;
-        for(int index=2;index<=n;index++){
-            int pick=nums[index-1]+prev2;
-            int nonpick=0+prev1;
-            ans=Math.max(pick,nonpick);
-
+        for(int state=2;state<=n;state++){
+            int pick=nums[state-1]+prev2;
+            int nopick=0+prev1;
+            ans=Math.max(pick,nopick);
             prev2=prev1;
             prev1=ans;
         }
         return ans;
-    }
-    public int rob(int[] nums) {
-        int n=nums.length;
-        if (n == 1) return nums[0];
-        int num1[]=new int[n-1];
-        int num2[]=new int[n-1];
-        int j=0,k=0;
-        for(int i=0;i<n;i++){
-           if(i!=0){
-            num1[j]=nums[i];
-            j++;
-           }if(i!=n-1){
-            num2[k]=nums[i];
-            k++;
-           }
-        }
-        return Math.max(rob1(num1),rob1(num2));
     }
 }
