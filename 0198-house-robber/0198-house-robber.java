4603f418b0d399fe738e46bf01b06188231e9c23
RiyaHablani <121542860+RiyaHablani@users.noreply.github.com>
2026-04-22 21:34:33 +0530

    Time: 0 ms (100%), Space: 42.5 MB (94.72%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,84 +1,16 @@
 class Solution {
-    // public int rob(int[] nums) {
-    //     int n=nums.length;
-    //     return rec(nums,n-1);
-    // }
-    // public int rec(int nums[],int index){
-    //     //base case
-    //     //TC:O(n),SC:O(n)
-    //     if(index==0){
-    //         return nums[0];
-    //     }if(index==-1){
-    //         return 0;
-    //     }
-    //     //pick and no pick logic
-    //     int pick=nums[index]+rec(nums,index-2);
-    //     int nonpick=0+rec(nums,index-1);
-    //     return Math.max(pick,nonpick);
-    // }
-
-
-//     public int rob(int[] nums) {
-//         int n=nums.length;
-//         int dp[]=new int[n+1];
-//         Arrays.fill(dp,-1);
-//         return rec(nums,n,dp);
-//     }
-//     public int rec(int nums[],int index,int dp[]){
-//         //base case
-//         //TC:O(n),SC:O(n)
-//         //shifting logic->index->+1,dp->as it is,nums->-1
-//         // shifting logic -> index is 1-based (dp[index] = max profit for first 'index' houses)
-// // so we access nums[index-1] when using house values
-
-//         if(index==1){
-//             dp[index]=nums[0];
-//             return nums[0];
-//         }if(index==0){
-//             dp[index]=0;
-//             return 0;
-//         }
-//         if(dp[index]!=-1){
-//             return dp[index];
-//         }
-//         //pick and no pick logic
-//         int pick=nums[index-1]+rec(nums,index-2,dp);
-//         int nonpick=0+rec(nums,index-1,dp);
-//         dp[index]=Math.max(pick,nonpick);
-//         return dp[index];
-//     }
-
-
-
-    // public int rob(int[] nums) {
-    //     //TC:O(n),SC:O(n)
-    //     int n=nums.length;
-    //     int dp[]=new int[n+1];
-    //     dp[0]=0;
-    //     dp[1]=nums[0];
-    //     for(int index=2;index<=n;index++){
-    //         int pick=nums[index-1]+dp[index-2];
-    //         int nonpick=0+dp[index-1];
-    //         dp[index]=Math.max(pick,nonpick);
-    //     }
-    //     return dp[n];
-    // }
-
-
-
     public int rob(int[] nums) {
-        //TC:O(n),SC:O(n)
         int n=nums.length;
-        if(n==1) return nums[0];
-        
+        if(n==1){
+            return nums[0];
+        }
         int prev2=0;
         int prev1=nums[0];
         int ans=0;
-        for(int index=2;index<=n;index++){
-            int pick=nums[index-1]+prev2;
-            int nonpick=0+prev1;
-            ans=Math.max(pick,nonpick);
-
+        for(int state=2;state<=n;state++){
+            int pick=nums[state-1]+prev2;
+            int nopick=0+prev1;
+            ans=Math.max(pick,nopick);
             prev2=prev1;
             prev1=ans;
         }
