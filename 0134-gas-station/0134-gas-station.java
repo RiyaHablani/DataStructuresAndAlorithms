4dd87ef4ef225b3872c32247cf4075b1011f40a0
RiyaHablani <121542860+RiyaHablani@users.noreply.github.com>
2026-05-15 21:32:53 +0530

    Time: 51 ms (5.08%), Space: 132.5 MB (9.8%) - LeetHub

--- 0134-gas-station/0134-gas-station.java
+++ 0134-gas-station/0134-gas-station.java
@@ -1,25 +1,53 @@
+class Station implements Comparable<Station> {
+    int cost;
+    int gas;
+    int index;
+
+    Station(int cost, int gas, int index) {
+        this.cost = cost;
+        this.gas = gas;
+        this.index = index;
+    }
+
+    public int compareTo(Station that) {
+        return (this.gas - this.cost) - (that.gas - that.cost);
+    }
+}
+
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
+
+        PriorityQueue<Station> minpq = new PriorityQueue<>();
+
+        for (int i = 0; i < cost.length; i++) {
+            minpq.offer(new Station(cost[i], gas[i], i));
+        }
+
         int totalGas = 0;
         int totalCost = 0;
+
+        while (!minpq.isEmpty()) {
+            Station curr = minpq.poll();
+
+            totalGas += curr.gas;
+            totalCost += curr.cost;
+        }
+
+        if (totalGas < totalCost) {
+            return -1;
+        }
+
         int tank = 0;
         int start = 0;
 
         for (int i = 0; i < gas.length; i++) {
-            totalGas += gas[i];
-            totalCost += cost[i];
+
             tank += gas[i] - cost[i];
 
-            // If tank goes negative, reset and try the next station
             if (tank < 0) {
                 start = i + 1;
                 tank = 0;
             }
-        }
-
-        // If total gas is less than total cost, cannot complete the circuit
-        if (totalGas < totalCost) {
-            return -1;
         }
 
         return start;
