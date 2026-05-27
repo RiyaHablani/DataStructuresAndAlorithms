fcae40bfa758618e3dc3346daad139ddd3e415c0
RiyaHablani <121542860+RiyaHablani@users.noreply.github.com>
2026-05-27 21:04:42 +0530

    Time: 0 ms (100%), Space: 43.9 MB (90.51%) - LeetHub

--- 2678-number-of-senior-citizens/2678-number-of-senior-citizens.java
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.java
@@ -2,8 +2,9 @@ class Solution {
     public int countSeniors(String[] details) {
         int count=0;
         for(String detail : details){
-            String age=detail.substring(11,13);
-            int ageVal=Integer.parseInt(age);
+            int a=detail.charAt(11)-'0';
+            int b=detail.charAt(12)-'0';
+            int ageVal=a*10+b;
             if(ageVal>60){
                 count++;
             }
