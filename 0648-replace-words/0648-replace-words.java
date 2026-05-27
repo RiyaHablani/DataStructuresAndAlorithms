bbb24fd81bae16bef2d0d231a29d5b56b763810b
RiyaHablani <121542860+RiyaHablani@users.noreply.github.com>
2026-05-27 20:56:46 +0530

    Time: 8 ms (99.73%), Space: 62.9 MB (57.75%) - LeetHub

--- 0648-replace-words/0648-replace-words.java
+++ 0648-replace-words/0648-replace-words.java
@@ -1,17 +1,60 @@
-class Solution {
-    public String replaceWords(List<String> dictionary, String sentence) {
-        String[] word = sentence.split(" ");
-        for (int i = 0; i < dictionary.size(); i++) {
-            for (int j = 0; j < word.length; j++) {
-                if (word[j].startsWith(dictionary.get(i))) {
-                    word[j] = dictionary.get(i);
-                }
+class Node{
+    Node child[];
+    boolean isWord;
+    Node(){
+        child=new Node[26];
+        isWord=false;
+    }
+}
+class Trie{
+    Node root;
+    Trie(){
+        root=new Node();
+    }
+    public void addWord(String word){
+        Node temp=root;
+        for(char ch:word.toCharArray()){
+            int index=ch-'a';
+            if(temp.child[index]==null){
+                temp.child[index]=new Node();
+            }
+            temp=temp.child[index];
+        }
+        temp.isWord=true;
+    }
+    public String findPrefix(String word){
+        Node temp=root;
+        for(int i=0;i<word.length();i++){
+            char ch=word.charAt(i);
+            int index=ch-'a';
+            if(temp.child[index]==null){
+                return "";
+            }
+            temp=temp.child[index];
+            if(temp.isWord){
+                return word.substring(0,i+1);
             }
         }
-        String value="";
-        for(String res : word){
-            value+=res+" ";
+        return "";
+    }
+}
+class Solution {
+    public String replaceWords(List<String> dictionary, String sentence) {
+        StringBuilder res=new StringBuilder();
+        Trie trie=new Trie();
+        for(String word:dictionary){
+            trie.addWord(word);
         }
-        return value.trim();
+        String words[]=sentence.split(" ");
+        for(String word:words){
+            String prefix=trie.findPrefix(word);
+            if(prefix==""){
+                res.append(word);
+            }else{
+                res.append(prefix);
+            }
+            res.append(" ");
+        }
+        return res.toString().trim();
     }
 }
