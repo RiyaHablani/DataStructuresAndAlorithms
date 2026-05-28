b5c2255d0a32911a9b517e1c6f3f8fb99888d0fc
RiyaHablani <121542860+RiyaHablani@users.noreply.github.com>
2026-05-29 01:43:31 +0530

    Time: 1 ms (99.67%), Space: 43 MB (30.82%) - LeetHub

---
+++ 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.java
@@ -0,0 +1,20 @@
+// two pass
+class Solution {
+    public int maxScore(String s) {
+        //find all the ones
+        int ones = 0;
+        int n = s.length();
+        for(int i=0;i<n;i++){
+            if(s.charAt(i) == '1') ones++;
+        }
+        // find max partition
+        int res = 0;
+        int zeros=0;
+        for(int i=0;i<n-1;i++){
+            if(s.charAt(i) == '0') zeros++;
+            else ones--; 
+            res = Math.max(res, ones + zeros);
+        }
+        return res;
+    }
+}
