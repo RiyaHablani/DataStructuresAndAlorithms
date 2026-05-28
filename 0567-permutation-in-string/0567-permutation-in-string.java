65c909195d292b15daa3c09347fe4147e27aadf4
RiyaHablani <121542860+RiyaHablani@users.noreply.github.com>
2026-05-28 17:33:19 +0530

    Time: 6 ms (86.61%), Space: 44.1 MB (49.94%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,19 +1,22 @@
-//two map comparision
+// Sliding Window
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
         int n = s1.length();
         int m = s2.length();
         if(m<n) return false;
-        int map1[] = new int[26];   //0
-        
+        int map1[] = new int[26];   
+        int map2[] = new int[26];
         for(int i=0;i<n;i++){
             map1[s1.charAt(i) - 'a']++;
-        }   
-        for(int i=0;i<=m-n;i++){
-            int map2[] = new int[26];
-            for(int j=0;j<n;j++){
-                map2[s2.charAt(i+j) - 'a']++;
-            }
+            map2[s2.charAt(i) - 'a']++;
+        }
+        //init window   
+        if(isMatched(map1,map2)){
+            return true;
+        }
+        for(int i=1;i<=m-n;i++){
+            map2[s2.charAt(i-1) - 'a']--; // del
+            map2[s2.charAt(i+n-1) - 'a']++;// add
             if(isMatched(map1,map2)){
                 return true;
             }
