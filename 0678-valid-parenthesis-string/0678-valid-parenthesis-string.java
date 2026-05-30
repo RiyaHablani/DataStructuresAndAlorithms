6341fab4f2fa4f015886869367fffe78564c0da4
RiyaHablani <121542860+RiyaHablani@users.noreply.github.com>
2026-05-30 20:50:22 +0530

    Time: 1 ms (23.56%), Space: 42.9 MB (31.59%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
@@ -1,23 +1,31 @@
 class Solution {
     public boolean checkValidString(String s) {
-        int min = 0, max = 0;
-
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (c == '(') {
-                min++;
-                max++;
-            } else if (c == ')') {
-                min--;
-                max--;
-            } else if (c == '*') {
-                min--;
-                max++;
+        Stack<Integer> stack1=new Stack<>();
+        Stack<Integer> stack2=new Stack<>();
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(ch=='('){
+                stack1.push(i);
+            }else if(ch=='*'){
+                stack2.push(i);
+            }else{
+                if(!stack1.isEmpty()){
+                    stack1.pop();
+                }else if(!stack2.isEmpty()){
+                    stack2.pop();
+                }else{
+                    return false;
+                }
             }
-
-            if (min < 0) min = 0;
-            if (max < 0) return false;
         }
-        return min == 0;
+        while(!stack1.isEmpty() && !stack2.isEmpty()){
+            if(stack2.isEmpty()){
+                return false;
+            }
+            if(stack1.pop()>stack2.pop()){
+                return false;
+            }
+        }
+        return stack1.isEmpty();
     }
 }
