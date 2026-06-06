1e86ba216c1caf27ab5c06a38c8504d77bfca2c5
RiyaHablani <121542860+RiyaHablani@users.noreply.github.com>
2026-06-07 02:49:47 +0530

    Time: 0 ms (100%), Space: 45.7 MB (40.27%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -18,12 +18,8 @@ class Solution {
         if(root==null){
             return true;
         }
-        int leftTreeDepth=maxDepth(root.left);
-        int rightTreeDepth=maxDepth(root.right);
-        if(Math.abs(leftTreeDepth - rightTreeDepth) > 1) {
-            return false;
-        }
-        return isBalanced(root.left) && isBalanced(root.right);
+        if(maxDepth(root)==-1) return false;
+        return true;
     }
     public int maxDepth(TreeNode root){
         if(root==null){
@@ -31,6 +27,12 @@ class Solution {
         }
         int leftTree=maxDepth(root.left);
         int rightTree=maxDepth(root.right);
+        if(leftTree==-1 || rightTree==-1){
+            return -1;
+        }
+        if(Math.abs(leftTree-rightTree)>1){
+            return -1;
+        }
         return 1+Math.max(leftTree, rightTree);
     }
 }
