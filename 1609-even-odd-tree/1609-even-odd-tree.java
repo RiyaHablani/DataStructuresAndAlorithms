83535b662db02257fb2dfdd9f60b5a5b04f9f54e
RiyaHablani <121542860+RiyaHablani@users.noreply.github.com>
2026-06-09 02:30:57 +0530

    Time: 12 ms (35.84%), Space: 71.9 MB (99.37%) - LeetHub

--- 1609-even-odd-tree/1609-even-odd-tree.java
+++ 1609-even-odd-tree/1609-even-odd-tree.java
@@ -17,32 +17,22 @@ class Solution {
     public boolean isEvenOddTree(TreeNode root) {
         Queue<TreeNode> queue=new LinkedList<>();
         queue.offer(root);
-        int level=0;
+        boolean isEven=true;
         while(!queue.isEmpty()){
             int size=queue.size();
             List<Integer> list=new ArrayList<>();
+            int prev=(isEven)?Integer.MIN_VALUE:Integer.MAX_VALUE;
             for(int i=0;i<size;i++){
                 TreeNode currentNode=queue.poll();
-                list.add(currentNode.val);
-                if(!list.isEmpty() && list.size()>1 && level%2==0){
-                    if(list.get(i-1)>=list.get(i)){
-                        return false;
-                    }
-                }if(!list.isEmpty() && list.size()>1 && level%2!=0){
-                    if(list.get(i)>=list.get(i-1)){
-                        return false;
-                    }
-                }
-                if(level%2==0 && list.get(i)%2==0){
+                if((isEven && (currentNode.val%2==0 || currentNode.val<=prev)) || 
+                (!isEven && (currentNode.val%2!=0 || currentNode.val>=prev))){
                     return false;
                 }
-                if(level%2!=0 && list.get(i)%2!=0){
-                    return false;
-                }
+                prev=currentNode.val;
                 if(currentNode.left!=null) queue.offer(currentNode.left);
                 if(currentNode.right!=null) queue.offer(currentNode.right);
             }
-            level++;
+            isEven=!isEven;
         }
         return true;
     }
