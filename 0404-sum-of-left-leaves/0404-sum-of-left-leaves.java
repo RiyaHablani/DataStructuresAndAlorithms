a0072c9c24fabfbfe03985f9c223d98a51b9ab3e
RiyaHablani <121542860+RiyaHablani@users.noreply.github.com>
2026-06-09 03:18:32 +0530

    Time: 0 ms (100%), Space: 43.4 MB (29.01%) - LeetHub

--- 0404-sum-of-left-leaves/0404-sum-of-left-leaves.java
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.java
@@ -19,12 +19,24 @@ class Solution {
         if(root==null){
             return 0;
         }
-        if(root.left!=null &&(root.left.left==null && root.left.right==null)){
-            sum+=root.left.val;
+        Queue<TreeNode> queue=new LinkedList<>();
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            int size=queue.size();
+            for(int i=0;i<size;i++){
+                TreeNode node=queue.poll();
+                if(node.left != null){
+                    if(node.left.left == null && node.left.right == null){
+                        sum += node.left.val;
+                    }else {
+                        queue.offer(node.left);
+                    }
+                }
+                if(node.right!=null){
+                    queue.offer(node.right);
+                }
+            }
         }
-        sum+=sumOfLeftLeaves(root.left);
-        sum+=sumOfLeftLeaves(root.right);
-
         return sum;
     }
 }
