04ec040a719197da14185193838cf41e3af22b99
RiyaHablani <121542860+RiyaHablani@users.noreply.github.com>
2026-08-10 19:57:24 +0530

    Time: 0 ms (100%), Space: 46.6 MB (17.11%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.java
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.java
@@ -10,29 +10,37 @@
  */
 class Solution {
     public ListNode reverseKGroup(ListNode head, int k) {
+        // Check whether we have at least k nodes
         ListNode curr = head;
-        int count = 0;
-        while (curr != null && count < k) {
-            curr = curr.next;
-            count++;
-        }
-        
-        if (count == k) {
-            ListNode prev = null;
-            ListNode next = null;
-            ListNode tail = head;
-            for (int i = 0; i < k; i++) {
-                next = head.next;
-                head.next = prev;
-                prev = head;
-                head = next;
+        for (int i = 0; i < k; i++) {
+            // If fewer than k nodes are left,
+            // don't reverse them
+            if (curr == null) {
+                return head;
             }
-            
-            tail.next = reverseKGroup(head, k);
-            
-            return prev;
+            curr = curr.next;
         }
-        
+        // 'curr' is now the first node of the next group
+        // 'prev' will eventually become the end
+        // of the reversed group
+        ListNode prev = head;
+        // Start reversing from the second node
+        ListNode nextNode = head.next;
+        // Reverse k nodes
+        for (int i = 0; i < k - 1; i++) {
+            // Save the next node before changing the link
+            ListNode temp = nextNode.next;
+            // Reverse the link
+            nextNode.next = head;
+            // Move head to the newly reversed node
+            head = nextNode;
+            // Move to the next node
+            nextNode = temp;
+        }
+        // 'prev' is now the last node of the reversed group.
+        // Connect it to the result of the next group.
+        prev.next = reverseKGroup(curr, k);
+        // 'head' is now the first node of the reversed group
         return head;
     }
 }
