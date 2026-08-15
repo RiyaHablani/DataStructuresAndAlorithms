6fdef06441513cc6fa46d2150a49b45aa411bfb4
RiyaHablani <121542860+RiyaHablani@users.noreply.github.com>
2026-08-15 22:23:06 +0530

    Time: 3 ms (86.11%), Space: 43.1 MB (73.54%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -3,22 +3,16 @@ class Solution {
         Stack<Character> stack=new Stack<>();
         for(int i=0;i<s.length();i++){
             char ch=s.charAt(i);
-            if(stack.isEmpty() || ch=='(' || ch=='[' || ch=='{'){
+            if(ch=='(' || ch=='{' || ch=='['){
                 stack.push(ch);
             }else{
-                if(!stack.isEmpty()){
-                    if(stack.peek()=='(' && ch==')'){
-                        stack.pop();
-                    }else if(stack.peek()=='[' && ch==']'){
-                        stack.pop();
-                    }else if(stack.peek()=='{' && ch=='}'){
-                        stack.pop();
-                    }else{
-                        stack.push(ch);
-                    }
+                if(stack.isEmpty()){
+                    return false;
+                }else if((stack.peek()=='[' && ch==']')|| (stack.peek()=='(' && ch==')') || (stack.peek()=='{' && ch=='}')){
+                    stack.pop();
                 }else{
-                    stack.push(ch);
-                }  
+                    return false;
+                }
             }
         }
         return stack.isEmpty();
