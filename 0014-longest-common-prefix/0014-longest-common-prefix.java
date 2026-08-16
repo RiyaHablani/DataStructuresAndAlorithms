a028b801d5b96630e9d4fdb913c54e361ee6b882
RiyaHablani <121542860+RiyaHablani@users.noreply.github.com>
2026-08-16 16:21:21 +0530

    Time: 1 ms (61.1%), Space: 43.1 MB (67.87%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -5,7 +5,7 @@ class Solution {
         String str1=strs[0];
         String str2=strs[strs.length-1];
         int i=0,j=0;
-        while(i < str1.length() && i < str2.length()){
+        while(i<str1.length() && i<str2.length()){
             if(str1.charAt(i)==str2.charAt(j)){
                 sb.append(str1.charAt(i));
                 i++;
