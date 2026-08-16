96c27830aedb4b5d9f0558e4e92b4dbd8cef4724
RiyaHablani <121542860+RiyaHablani@users.noreply.github.com>
2026-08-16 20:22:05 +0530

    Time: 0 ms (100%), Space: 42.9 MB (42.09%) - LeetHub

--- 0165-compare-version-numbers/0165-compare-version-numbers.java
+++ 0165-compare-version-numbers/0165-compare-version-numbers.java
@@ -1,39 +1,25 @@
 class Solution {
     public int compareVersion(String version1, String version2) {
-        // Split the version strings by "."
-        String[] v1 = version1.split("\\.");
-        String[] v2 = version2.split("\\.");
-        
-        // Find the maximum length of the two version arrays
-        int maxLength = Math.max(v1.length, v2.length);
-        
-        // Iterate through all revision indices
-        for (int i = 0; i < maxLength; i++) {
-            int num1, num2;
-
-            // Check if index 'i' is within the bounds of v1
-            if (i < v1.length) {
-                num1 = Integer.parseInt(v1[i]); // Convert to integer
-            } else {
-                num1 = 0; // If out of bounds, assume 0
+        int n1=version1.length();
+        int n2=version2.length();
+        int p1=0,p2=0;
+        while(p1<n1 || p2<n2){
+            int num1=0;
+            while(p1<n1 && version1.charAt(p1)!='.'){
+                num1=num1*10+(version1.charAt(p1)-'0');
+                p1++;
             }
-
-            // Check if index 'i' is within the bounds of v2
-            if (i < v2.length) {
-                num2 = Integer.parseInt(v2[i]); // Convert to integer
-            } else {
-                num2 = 0; // If out of bounds, assume 0
+            int num2=0;
+            while(p2<n2 && version2.charAt(p2)!='.'){
+                num2=num2*10+(version2.charAt(p2)-'0');
+                p2++;
             }
-
-            // Compare the current revision numbers
-            if (num1 > num2) {
-                return 1; // version1 is greater
-            } else if (num1 < num2) {
-                return -1; // version2 is greater
+            if(num1!=num2){
+                return (num1>num2)?1:-1;
             }
+            p1++;
+            p2++;
         }
-        
-        // Both versions are equal
         return 0;
     }
 }
