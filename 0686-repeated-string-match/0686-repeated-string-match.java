982d58b1ee69f4064bc51133987a8a506c7fb373
RiyaHablani <121542860+RiyaHablani@users.noreply.github.com>
2026-08-16 16:40:25 +0530

    Time: 267 ms (30.08%), Space: 43.7 MB (57.37%) - LeetHub

--- 0686-repeated-string-match/0686-repeated-string-match.java
+++ 0686-repeated-string-match/0686-repeated-string-match.java
@@ -1,18 +1,16 @@
 class Solution {
     public int repeatedStringMatch(String a, String b) {
-        StringBuilder sb = new StringBuilder(a);
-        int count = 1;  // a is already included once
-
-        while (sb.length() < b.length()) {
+        StringBuilder sb=new StringBuilder(a);
+        int count=1;
+        while(sb.length()<b.length()){
             sb.append(a);
             count++;
         }
-
-        if (sb.toString().contains(b)) return count;
-
+        if(sb.toString().contains(b)){
+            return count;
+        }
         sb.append(a);
         count++;
-
-        return sb.toString().contains(b) ? count : -1;
+        return sb.toString().contains(b)?count:-1;
     }
 }
