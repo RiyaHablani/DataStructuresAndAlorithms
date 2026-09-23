d37c4360c52def56a2b394819ac098dc2b531671
RiyaHablani <121542860+RiyaHablani@users.noreply.github.com>
2026-09-23 16:51:56 +0530

    Time: 2 ms (89.63%), Space: 48 MB (42.42%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,14 +1,19 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int n=nums.length;
-        int i=0;
-        int maxIndex=0;
-        while(i<=maxIndex){
-            maxIndex=Math.max(maxIndex,i+nums[i]);
-            if(maxIndex>=n-1){
+        // Farthest index we can reach
+        int maxReach = 0;
+        for (int i = 0; i < nums.length; i++) {
+            // If current index is beyond our reach,
+            // we cannot move further
+            if (i > maxReach) {
+                return false;
+            }
+            // Find the farthest place we can reach
+            maxReach = Math.max(maxReach, i + nums[i]);
+            // If we can reach the last index
+            if (maxReach >= nums.length - 1) {
                 return true;
             }
-            i++;
         }
         return false;
     }
